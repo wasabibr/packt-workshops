49001b1e4a45846e5afe15168c4561878053920a
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-09-07 22:15:55 -0300

    inserido sql-cap-01-act_1.01-inserting_values_into_the_products_table_in_the_POS-DB.sql

---
+++ mySQL/sql-cap-01-act_1.01-inserting_values_into_the_products_table_in_the_POS-DB.sql
@@ -0,0 +1,15 @@
+USE PACKT_ONLINE_SHOP;
+INSERT INTO Customers (FirstName, MiddleName, LastName, HomeAddress, Email, Phone,
+Notes)
+VALUES('Joe', 'Greg', 'Smith', '2356 Elm St.', 'joesmith@sfghwert.com', '(310)
+555-1212', 'Always gets products home delivered'),
+('Grace', 'Murray', 'Hopper', '123 Compilation Street', 'gmhopper@ftyuw46.com',
+'(818) 555-3678', 'Compiler pioneer'),
+('Ada', NULL, 'Lovelace', '22 Algorithm Way', 'adalovelace@fgjw54af.gov', '(717)
+555-3457', 'First software engineer'),
+('Joseph', 'Force', 'Crater', '1313 Mockingbird Lane', 'judgecrater@ev56gfwrty.com',
+'(212) 555-5678', 'Works everyday'),
+('Jacqueline', 'Jackie', 'Cochran', '1701 Flightspeed Avenue', 'jackiecochrane@
+jryuwp8qe4w.gov', '(717) 555-3457', 'Researcher'),
+(NULL, 'Paul', 'Jones', '126 Bonhomme Richard Ave.', 'jpjones@bonhommerichard.edu',
+'(216) 555-6232', 'Admiral');
