c554b3597c5806326d73d59ba7b79235d73adfe0
Jefferson Leandro Ramos de Oliveira <34520708+wasabibr@users.noreply.github.com>
2022-09-07 22:04:00 -0300

    inserido sql-cap-01-exe_1.01-building_the_POS-DB.sql

---
+++ mySQL/sql-cap-01-exe_1.01-building_the_POS-DB.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE PACKT_ONLINE_SHOP;
+USE PACKT_ONLINE_SHOP;
+CREATE TABLE Customers
+(
+    FirstName varchar(50) ,
+    MiddleName varchar(50) ,
+    LastName varchar(50) ,
+    HomeAddress varchar(250) ,
+    Email varchar(200) ,
+    Phone varchar(50) ,
+    Notes varchar(250)
+);
